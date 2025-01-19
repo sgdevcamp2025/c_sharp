f5de8f51f6aed482fbc76ad089d7f1f98f131b43
KimKyuHoi <k546kh@gmail.com>
2025-01-20 02:45:16 +0900

    #3 build(fe): 절대 경로 설정

--- src/frontend/apps/web/app/page.tsx
+++ src/frontend/apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import Image, { type ImageProps } from 'next/image';
-import { Button } from '@jootalkpia/ui/button';
+import { Button } from '@jootalkpia/ui';
 import styles from './page.module.css';
 
 type Props = Omit<ImageProps, 'src'> & {
@@ -63,9 +63,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
+        <Button>Open alert</Button>
       </main>
       <footer className={styles.footer}>
         <a
