bd4cd67ab9fda3e862779552df1905a66e673034
KimSehoon <shggm2000@gmail.com>
2025-01-21 13:54:53 +0900

    #21 feat(be): from mapper method 구현
    - 팩토리 메서드 적용
    - minutePriceDto를 MinutePriceResponse로 변경

--- src/backend/stock_server/src/main/java/com/example/stock_server/stocks/dto/response/MinutePriceResponse.java
+++ src/backend/stock_server/src/main/java/com/example/stock_server/stocks/dto/response/MinutePriceResponse.java
@@ -1,5 +1,7 @@
 package com.example.stock_server.stocks.dto.response;
 
+import com.example.stock_server.stocks.dto.MinutePriceDto;
+
 public record MinutePriceResponse(
         String code,
         String hts_kor_isnm,
@@ -12,4 +14,19 @@ public record MinutePriceResponse(
         String cntg_vol,
         String acml_tr_pbmn
 ) {
+
+    public static MinutePriceResponse from(MinutePriceDto minutePriceDto, String code) {
+        return new MinutePriceResponse(
+                code,
+                minutePriceDto.output1().hts_kor_isnm(),
+                minutePriceDto.output2().get(1).stck_bsop_date(),
+                minutePriceDto.output2().get(1).stck_cntg_hour(),
+                minutePriceDto.output2().get(1).stck_prpr(),
+                minutePriceDto.output2().get(1).stck_oprc(),
+                minutePriceDto.output2().get(1).stck_hgpr(),
+                minutePriceDto.output2().get(1).stck_lwpr(),
+                minutePriceDto.output2().get(1).cntg_vol(),
+                minutePriceDto.output2().get(1).acml_tr_pbmn()
+        );
+    }
 }
