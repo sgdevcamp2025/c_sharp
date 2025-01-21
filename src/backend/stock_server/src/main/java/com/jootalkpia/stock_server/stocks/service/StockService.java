e5c8f9f2147031809eeef11444672bf04cfc6230
KimSehoon <shggm2000@gmail.com>
2025-01-21 20:58:46 +0900

    #41 feat(be): refreshToken method 구현
    - BEARER 상수 구현
    - token 전역 변수 설정
    - Scheduled를 이용해 자정마다 토큰 재발급

--- src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/service/StockService.java
+++ src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/service/StockService.java
@@ -1,18 +1,31 @@
 package com.jootalkpia.stock_server.stocks.service;
 
 import com.jootalkpia.stock_server.stocks.advice.StockCaller;
+import com.jootalkpia.stock_server.stocks.dto.request.TokenRequestBody;
 import com.jootalkpia.stock_server.stocks.dto.response.MinutePriceResponse;
 import com.jootalkpia.stock_server.support.property.BaseProperties;
 import com.jootalkpia.stock_server.support.property.MinutePriceProperties;
+import com.jootalkpia.stock_server.support.property.TokenProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class StockService {
+    private static final String BEARER = "Bearer ";
+
+    private String token;
+
     private final StockCaller stockCaller;
     private final BaseProperties baseProperties;
     private final MinutePriceProperties minutePriceProperties;
+    private final TokenProperties tokenProperties;
+
+    @Scheduled(cron = "0 0 0 * **")
+    public void refreshToken() {
+        token = BEARER + stockCaller.getToken(TokenRequestBody.from(baseProperties, tokenProperties)).accessToken();
+    }
 
     public MinutePriceResponse getMinutePriceDto(String token, String code, String time) {
         return stockCaller.getMinutePrice(
