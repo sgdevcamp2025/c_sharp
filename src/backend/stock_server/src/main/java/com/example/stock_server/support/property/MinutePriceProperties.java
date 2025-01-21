027630f846a9e9d93bd194b3e6e7fe0a0c4395ca
KimSehoon <shggm2000@gmail.com>
2025-01-21 10:50:31 +0900

    #21 feat(be): MinutePriceProperties record 구현
    - 당일 일분봉 URL
    - 거래ID
    - 고객 타입
    - 3개 다 불변 값

---
+++ src/backend/stock_server/src/main/java/com/example/stock_server/support/property/MinutePriceProperties.java
@@ -0,0 +1,11 @@
+package com.example.stock_server.support.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "feign.minute-price")
+public record MinutePriceProperties(
+        String path,
+        String trId,
+        String custtype
+) {
+}
