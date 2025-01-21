5aee26282fadd538685e91bddea1d3ecc8783f1d
KimSehoon <shggm2000@gmail.com>
2025-01-21 11:05:35 +0900

    #21 feat(be): ToeknProperties record 구현
    - 토큰발급 URI
    - 권한 부여 타입

---
+++ src/backend/stock_server/src/main/java/com/example/stock_server/support/property/TokenProperties.java
@@ -0,0 +1,10 @@
+package com.example.stock_server.support.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "feign.stock-token")
+public record TokenProperties(
+        String path,
+        String grantType
+) {
+}
