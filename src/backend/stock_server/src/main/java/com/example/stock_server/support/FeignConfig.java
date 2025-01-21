296e01706f77c9417adc8c8a5e3f61bf3295b1ff
KimSehoon <shggm2000@gmail.com>
2025-01-21 11:11:06 +0900

    #21 feat(be): FeignConfig 설정
    - logger.level.FULL

---
+++ src/backend/stock_server/src/main/java/com/example/stock_server/support/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.example.stock_server.support;
+
+import feign.Logger;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class FeignConfig {
+    @Bean
+    public feign.Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
