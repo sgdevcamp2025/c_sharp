4e5eecf20235cc3267812dee584da74ddfbef942
KimSehoon <shggm2000@gmail.com>
2025-01-21 20:57:22 +0900

    #41 feat(be): from mapper method 구현

--- src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/dto/request/TokenRequestBody.java
+++ src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/dto/request/TokenRequestBody.java
@@ -1,10 +1,20 @@
 package com.jootalkpia.stock_server.stocks.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jootalkpia.stock_server.support.property.BaseProperties;
+import com.jootalkpia.stock_server.support.property.TokenProperties;
 
 public record TokenRequestBody(
         @JsonProperty("grant_type") String grantType,
         @JsonProperty("appkey") String appKey,
         @JsonProperty("appsecret") String appSecret
 ) {
+
+    public static TokenRequestBody from(BaseProperties baseProperties, TokenProperties tokenProperties) {
+        return new TokenRequestBody(
+                tokenProperties.grantType(),
+                baseProperties.appKey(),
+                baseProperties.appSecret()
+        );
+    }
 }
