5059e627ff071363feff00e7f8960c6dfcbbcb2e
Sonjieun <sonjieun2661@gmail.com>
2025-01-22 01:21:04 +0900

    #6 fix(fe): 아바타 스토리북 타입 수정

--- src/frontend/packages/ui/src/components/Avatar/avatar.stories.tsx
+++ src/frontend/packages/ui/src/components/Avatar/avatar.stories.tsx
@@ -22,20 +22,20 @@ const meta: Meta<typeof Avatar> = {
 export default meta;
 type Story = StoryObj<typeof Avatar>;
 
-const Template: StoryFn = (args) => (
+const Template: StoryFn<typeof Avatar> = (args) => (
   <Avatar {...args}>
-    <AvatarImage src="https://github.com/shadcn.png" alt={args.children} />
+    <AvatarImage src="https://github.com/shadcn.png" />
     <AvatarFallback>{args.children}</AvatarFallback>
   </Avatar>
 );
 
-export const Default = Template.bind({});
+export const Default: Story = Template.bind({});
 Default.args = {
   variant: 'default',
   children: 'Avatar',
 };
 
-export const Logo = Template.bind({});
+export const Logo: Story = Template.bind({});
 Logo.args = {
   variant: 'square',
   children: 'Logo',
