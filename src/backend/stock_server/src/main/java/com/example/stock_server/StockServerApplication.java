10896b4d5237233eb03e9470ef4f900cf81a07b8
KimSehoon <shggm2000@gmail.com>
2025-01-21 10:45:45 +0900

    #21 feat(be): @EnableFeignClients 추가

--- src/backend/stock_server/src/main/java/com/example/stock_server/StockServerApplication.java
+++ src/backend/stock_server/src/main/java/com/example/stock_server/StockServerApplication.java
@@ -2,7 +2,11 @@ package com.example.stock_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class StockServerApplication {
 
