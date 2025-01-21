cd3d691ae3c7e3de3405cb282b75cf441e52c84f
KimSehoon <shggm2000@gmail.com>
2025-01-21 13:52:57 +0900

    #21 style(be): getMinutePrice -> getMinutePriceDto

--- src/backend/stock_server/src/main/java/com/example/stock_server/stocks/service/MinutePriceService.java
+++ src/backend/stock_server/src/main/java/com/example/stock_server/stocks/service/MinutePriceService.java
@@ -1,7 +1,7 @@
 package com.example.stock_server.stocks.service;
 
-import com.example.stock_server.stocks.MinutePriceCaller;
-import com.example.stock_server.stocks.MinutePriceResponse;
+import com.example.stock_server.stocks.advice.MinutePriceCaller;
+import com.example.stock_server.stocks.dto.MinutePriceDto;
 import com.example.stock_server.support.property.BaseProperties;
 import com.example.stock_server.support.property.MinutePriceProperties;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +14,7 @@ public class MinutePriceService {
     private final BaseProperties baseProperties;
     private final MinutePriceProperties minutePriceProperties;
 
-    public MinutePriceResponse getMinutePrice(String token, String code, String time) {
+    public MinutePriceDto getMinutePriceDto(String token, String code, String time) {
         return minutePriceCaller.getMinutePrice(
                 token,
                 baseProperties.appKey(),
