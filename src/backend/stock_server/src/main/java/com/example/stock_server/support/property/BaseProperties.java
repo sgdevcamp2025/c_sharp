1cb414f0c413824d9627d027d712c54ed454c607
KimSehoon <shggm2000@gmail.com>
2025-01-21 10:48:31 +0900

    #21 feat(be): BaseProperties record 구현
    - 기본 URI
    - appKey
    - appSecret
    - 3개의 값 모두 불변 값

---
+++ src/backend/stock_server/src/main/java/com/example/stock_server/support/property/BaseProperties.java
@@ -0,0 +1,11 @@
+package com.example.stock_server.support.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "feign")
+public record BaseProperties(
+        String baseUrl,
+        String appKey,
+        String appSecret
+) {
+}
