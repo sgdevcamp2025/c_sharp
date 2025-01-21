123a38fedc43e9eb6c5ea81daac301465f5742eb
KimSehoon <shggm2000@gmail.com>
2025-01-21 20:57:42 +0900

    #41 feat(be): stockCaller 내 getToken method 구현

--- src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/advice/StockCaller.java
+++ src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/advice/StockCaller.java
@@ -1,14 +1,17 @@
 package com.jootalkpia.stock_server.stocks.advice;
 
+import com.jootalkpia.stock_server.stocks.dto.request.TokenRequestBody;
 import com.jootalkpia.stock_server.stocks.dto.response.MinutePriceResponse;
+import com.jootalkpia.stock_server.stocks.dto.response.TokenResponse;
 import com.jootalkpia.stock_server.support.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "stockAPI",url = "${feign.base-url}", configuration = FeignConfig.class)
 public interface StockCaller {
+    @PostMapping(name = "getTokenAPI", value = "${feign.stock-token.path}")
+    TokenResponse getToken(@RequestBody TokenRequestBody tokenRequestBody);
+
     @GetMapping(name = "MinutePriceAPI", value = "${feign.minute-price.path}")
     MinutePriceResponse getMinutePrice(@RequestHeader(name = "authorization") String token,
                                        @RequestHeader(name = "appkey") String appKey,
