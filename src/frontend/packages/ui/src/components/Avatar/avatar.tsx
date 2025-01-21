c939c8bac44c7e7cc1f8dda8a5e5a4615b691752
Sonjieun <sonjieun2661@gmail.com>
2025-01-21 17:00:29 +0900

    #6 feat(fe): avatar 추가

---
+++ src/frontend/packages/ui/src/components/Avatar/avatar.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import * as React from 'react';
+import * as AvatarPrimitive from '@radix-ui/react-avatar';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@workspace/ui/lib/utils';
+
+const avatarVariants = cva('relative flex h-10 w-10 shrink-0 overflow-hidden', {
+  variants: {
+    variant: {
+      default: 'rounded-full',
+      square: 'rounded-md', // 로고용
+    },
+  },
+  defaultVariants: {
+    variant: 'square',
+  },
+});
+interface AvatarProps
+  extends React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>,
+    VariantProps<typeof avatarVariants> {}
+
+const Avatar = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  AvatarProps
+>(({ className, variant, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn(avatarVariants({ variant }), className)}
+    {...props}
+  />
+));
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+const AvatarImage = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Image>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={cn('aspect-square h-full w-full', className)}
+    {...props}
+  />
+));
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+const AvatarFallback = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Fallback>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      'flex h-full w-full items-center justify-center bg-muted',
+      className
+    )}
+    {...props}
+  />
+));
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+export { Avatar, AvatarImage, AvatarFallback };
--- src/frontend/packages/ui/src/components/index.ts
+++ src/frontend/packages/ui/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './Button';
 export * from './Badge';
 export * from './Textarea';
+export * from './Avatar';
