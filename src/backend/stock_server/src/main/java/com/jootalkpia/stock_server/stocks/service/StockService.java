13bb43e2f3e3654f00927b9104f36425b538029b
KimSehoon <shggm2000@gmail.com>
2025-01-22 16:46:31 +0900

    #42 refactor(be): refreshToken method

--- src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/service/StockService.java
+++ src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/service/StockService.java
@@ -3,19 +3,26 @@ package com.jootalkpia.stock_server.stocks.service;
 import com.jootalkpia.stock_server.stocks.advice.StockCaller;
 import com.jootalkpia.stock_server.stocks.dto.request.TokenRequestBody;
 import com.jootalkpia.stock_server.stocks.dto.response.MinutePriceResponse;
+import com.jootalkpia.stock_server.stocks.dto.response.MinutePriceSimpleResponse;
+import com.jootalkpia.stock_server.stocks.dto.response.TokenResponse;
 import com.jootalkpia.stock_server.support.property.BaseProperties;
 import com.jootalkpia.stock_server.support.property.MinutePriceProperties;
 import com.jootalkpia.stock_server.support.property.TokenProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class StockService {
-    private static final String BEARER = "Bearer ";
+    private static final String TOKEN_SEPARATOR = " ";
 
     private String token;
+
+    //테스트용, 토큰 1일 발급 횟수 제한
+    private String fakeToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6ImE5YzBlOTQ2LTdkYjMtNDBiNS1iYzRmLTViNGI2NzM3MzBlMyIsInByZHRfY2QiOiIiLCJpc3MiOiJ1bm9ndyIsImV4cCI6MTczNzYwNTY4MiwiaWF0IjoxNzM3NTE5MjgyLCJqdGkiOiJQU3B2NHhVejRpNkZzZzRhWENyTlY3VDJhN3JiZWdldmJKTDgifQ.8rnxngtudIEBBZLKiIM4Iq83fHnoxSnjpdxQRDOgvQ_1mPaNbRdo7PReDcJHiQsE34ltOU-g9Rvea9eElWwTcQ";
 
     private final StockCaller stockCaller;
     private final BaseProperties baseProperties;
@@ -23,13 +30,14 @@ public class StockService {
     private final TokenProperties tokenProperties;
 
     @Scheduled(cron = "0 0 0 * **")
-    public void refreshToken() {
-        token = BEARER + stockCaller.getToken(TokenRequestBody.from(baseProperties, tokenProperties)).accessToken();
+    private void refreshToken() {
+        TokenResponse tokenResponse = stockCaller.getToken(TokenRequestBody.from(baseProperties, tokenProperties));
+        token = tokenResponse.tokenType() + TOKEN_SEPARATOR + tokenResponse.accessToken();
     }
 
-    public MinutePriceResponse getMinutePriceDto(String token, String code, String time) {
-        return stockCaller.getMinutePrice(
-                token,
+    private MinutePriceSimpleResponse getStockPrice(String code, String time) {
+        MinutePriceResponse response = stockCaller.getMinutePrice(
+                fakeToken,
                 baseProperties.appKey(),
                 baseProperties.appSecret(),
                 minutePriceProperties.trId(),
@@ -40,5 +48,7 @@ public class StockService {
                 time,
                 minutePriceProperties.pwDataIncuYn()
         );
+
+        return MinutePriceSimpleResponse.from(response, code);
     }
 }
