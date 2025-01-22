aef6b023b45f41c3a7dcaa8a44d266096fc59be9
KimSehoon <shggm2000@gmail.com>
2025-01-22 16:56:10 +0900

    #42 test(be): 데몬 스레드 테스트를 위한 StockController 로직 변경
    - 1분마다 실행되는 각 작업의 스레드와 클라이언트 요청 시 실행되는 스레드의 비교를 위한 로직
    - dev branch와 충돌 방지

--- src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/controller/StockController.java
+++ src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/controller/StockController.java
@@ -15,12 +15,7 @@ public class StockController {
     private final StockService stockService;
 
     @GetMapping("")
-    public ResponseEntity<MinutePriceSimpleResponse> handleMinutePrice() {
-        //자동화 과정 필수
-        String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjQ5ZWUzMDhmLTMzMDUtNDM3Yi04OTQ3LWE4ZTRmZWNhNDk5ZCIsInByZHRfY2QiOiIiLCJpc3MiOiJ1bm9ndyIsImV4cCI6MTczNzQ1OTg5MSwiaWF0IjoxNzM3MzczNDkxLCJqdGkiOiJQU21sVzVod1pFVjFJNm1uMkoxMFZxbWVZSm1yTHdHTUF2bloifQ.qTzs1CcSIthQ6SgXgjzUxgL9v_S40pFDfPzF15iWNZ2BEPpehaRkBARv1UiUYukmNXFMtaMa23f-owSwuqUaOg";
-        String code = "005930";
-        String time = "100000";
-
-        return ResponseEntity.ok(MinutePriceSimpleResponse.from(stockService.getMinutePriceDto(token, code, time), code));
+    public ResponseEntity<String> handleMinutePrice() {
+        return ResponseEntity.ok("hi");
     }
 }
