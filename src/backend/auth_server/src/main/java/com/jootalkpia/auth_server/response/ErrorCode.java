ac2c5bb9ca2014f06bae6ba58526e98e92366582
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-01-23 02:43:40 +0900

    #26 feat(be): jwt관련 에러코드 및 yml 추가

--- src/backend/auth_server/src/main/java/com/jootalkpia/auth_server/response/ErrorCode.java
+++ src/backend/auth_server/src/main/java/com/jootalkpia/auth_server/response/ErrorCode.java
@@ -8,25 +8,31 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     // 400 Bad Request
-    ENUM_VALUE_BAD_REQUEST("40000", HttpStatus.BAD_REQUEST, "요청한 값이 유효하지 않습니다."),
-    BAD_REQUEST("40004", HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    MISSING_REQUIRED_HEADER("40005", HttpStatus.BAD_REQUEST, "필수 헤더가 누락되었습니다."),
-    MISSING_REQUIRED_PARAMETER("40006", HttpStatus.BAD_REQUEST, "필수 파라미터가 누락되었습니다."),
+    ENUM_VALUE_BAD_REQUEST("A40000", HttpStatus.BAD_REQUEST, "요청한 값이 유효하지 않습니다."),
+    BAD_REQUEST("A40001", HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    MISSING_REQUIRED_HEADER("A40002", HttpStatus.BAD_REQUEST, "필수 헤더가 누락되었습니다."),
+    MISSING_REQUIRED_PARAMETER("A40003", HttpStatus.BAD_REQUEST, "필수 파라미터가 누락되었습니다."),
 
     // 401 Unauthorized
+    ACCESS_TOKEN_EXPIRED("A40100", HttpStatus.UNAUTHORIZED, "액세스 토큰이 만료되었습니다."),
+    REFRESH_TOKEN_EXPIRED("A40101", HttpStatus.UNAUTHORIZED, "리프레시 토큰이 만료되었습니다."),
+    TOKEN_INCORRECT_ERROR("A40102", HttpStatus.UNAUTHORIZED, "리프레시 토큰이 유효하지 않습니다."),
+    EMPTY_PRINCIPAL("A40103", HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    REFRESH_TOKEN_NOT_FOUND("A40104", HttpStatus.UNAUTHORIZED, "해당 유저의 리프레시 토큰이 존재하지 않습니다."),
+
 
     //403 Forbidden
 
     // 404 Not Found
-    NOT_FOUND_END_POINT("40400", HttpStatus.NOT_FOUND, "존재하지 않는 API입니다."),
+    NOT_FOUND_END_POINT("A40400", HttpStatus.NOT_FOUND, "존재하지 않는 API입니다."),
 
     // 405 Method Not Allowed Error
-    METHOD_NOT_ALLOWED("40500", HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 메소드입니다."),
+    METHOD_NOT_ALLOWED("A40500", HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 메소드입니다."),
 
     // 422 Unprocessable Entity
 
     // 500 Internal Server Error
-    INTERNAL_SERVER_ERROR("50000", HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
+    INTERNAL_SERVER_ERROR("A50000", HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
 
     ;
 
