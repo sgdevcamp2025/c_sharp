022b0ea2affec31dbfde926c1526b009fe5c1c10
KimSehoon <shggm2000@gmail.com>
2025-01-22 16:25:34 +0900

    #49 feat(be): FeignConfig 내 objectMapper method 추가
    - json 데이터 변환 시 자동으로 스네이크 케이스로 변환
    - 자바의 기본 형식이 카멜 케이스라서 PropertyNamingStrategy 전략에 없는 것 같음
    - PropertyNamingStrategy 전략
        // 기본값 (변환 안함)
        LOWER_CAMEL_CASE
    
        // snake_case 변환
        SNAKE_CASE
    
        // kebab-case 변환
        KEBAB_CASE
    
        // 대문자 SNAKE_CASE 변환
        UPPER_SNAKE_CASE
    
        // 소문자로 변환
        LOWER_CASE
    
        // 대문자로 변환
        UPPER_CASE

--- src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/support/config/FeignConfig.java
+++ src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/support/config/FeignConfig.java
@@ -1,5 +1,7 @@
 package com.jootalkpia.stock_server.support.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import feign.Logger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +13,12 @@ public class FeignConfig {
     @Bean
     public feign.Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        return mapper;
     }
 }
