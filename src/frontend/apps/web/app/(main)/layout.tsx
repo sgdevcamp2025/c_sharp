8653896f092f7a2953ee371cb725476f82908e83
KimKyuHoi <k546kh@gmail.com>
2025-01-28 08:15:20 +0900

    #65 feat(fe): 전체 레이아웃 내 메인페이지 layout 설정하기

---
+++ src/frontend/apps/web/app/(main)/layout.tsx
@@ -0,0 +1,19 @@
+import '@workspace/ui/globals.css';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body>
+        <header className="h-[68px] bg-gray-800 flex items-center justify-center text-white">Header</header>
+        <div className="flex flex-col h-[calc(100vh-68px)] overflow-hidden">{children}</div>
+      </body>
+    </html>
+  );
+}
---
+++ src/frontend/apps/web/app/(main)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@workspace/ui/components';
+import Link from 'next/link';
+
+export default function Page() {
+  // 버튼 예시
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Hello World</h1>
+        <Button size="sm">
+          <Link href="/tesla">Button</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
