42696a685d19b1b7c20edd9b87eea57aea970508
KimSehoon <shggm2000@gmail.com>
2025-01-29 03:36:26 +0900

    #77 feat(be): CODE_TO_NAME_MAP method 구현
    - 클라이언트에게 받은 code를 통해 이름을 얻기 위한 로직
    - 반복문을 통해 enum 값을 조회할 수 있지만 확장성을 고려해 시간 복잡도를 최소화

--- src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/domain/StockCode.java
+++ src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/domain/StockCode.java
@@ -1,19 +1,39 @@
 package com.jootalkpia.stock_server.stocks.domain;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum StockCode {
-    SAMSUNG_ELECTRONICS("005930"),
-    KAKAO("035720"),
-    SK_HYNIX("000660"),
-    NAVER("035420"),
-    HANWHA_AEROSPACE("012450");
+    SAMSUNG_ELECTRONICS("005930", "삼성전자"),
+    KAKAO("035720", "카카오"),
+    SK_HYNIX("000660", "SK하이닉스"),
+    NAVER("035420", "네이버"),
+    HANWHA_AEROSPACE("012450", "한화에어로스페이스");
 
     private final String code;
+    private final String name;
 
-    StockCode(String code) {
+    private static final Map<String, String> CODE_TO_NAME_MAP = Arrays.stream(values())
+            .collect(Collectors.toMap(
+                    StockCode::getCode,
+                    StockCode::getName
+            ));
+
+    StockCode(String code, String name) {
         this.code = code;
+        this.name = name;
     }
 
     public String getCode() {
         return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static String getNameByCode(String code) {
+        return CODE_TO_NAME_MAP.get(code);
     }
 }
