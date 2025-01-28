cf0b0665035fa5fba9c758bc04b55df5490600b6
Sonjieun <sonjieun2661@gmail.com>
2025-01-28 21:02:46 +0900

    #59 feat(fe): 닉네임, 프로필사진 배치

--- src/frontend/apps/web/src/shared/components/header/profile-container.tsx
+++ src/frontend/apps/web/src/shared/components/header/profile-container.tsx
@@ -1,11 +1,15 @@
 import { Button } from '@workspace/ui/components';
+import ProfilePicture from './profile-picture';
+import Nickname from './nickname';
 
 const ProfileContainer = () => {
   return (
     <div className="flex flex-col items-start space-y-4">
       <h4 className="font-semibold leading-none">My Account</h4>
-      {/* 프로필사진관리 */}
-      {/* 닉네임 관리 */}
+      <div className="flex flex-col items-center space-y-4">
+        <ProfilePicture />
+        <Nickname />
+      </div>
       <Button
         variant="outline"
         size="sm"
