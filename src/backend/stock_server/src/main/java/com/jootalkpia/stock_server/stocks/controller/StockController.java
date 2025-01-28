6ccca82298e46beb1b9012995b24cc75b0beec49
KimSehoon <shggm2000@gmail.com>
2025-01-29 03:45:11 +0900

    #77 feat(be): handleCandlePriceHistory method 구현

--- src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/controller/StockController.java
+++ src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/controller/StockController.java
@@ -1,21 +1,22 @@
 package com.jootalkpia.stock_server.stocks.controller;
 
-import com.jootalkpia.stock_server.stocks.dto.response.MinutePriceSimpleResponse;
+import com.jootalkpia.stock_server.stocks.dto.response.CandlePriceHistoryResponse;
 import com.jootalkpia.stock_server.stocks.service.StockService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class StockController {
     private final StockService stockService;
 
-    @GetMapping("")
-    public ResponseEntity<String> handleMinutePrice() {
-        return ResponseEntity.ok("hi");
+    @GetMapping("/api/v1/stock/candlesticks/{stock_code}")
+    public ResponseEntity<CandlePriceHistoryResponse> handleCandlePriceHistory(@PathVariable("stock_code") String code, @PageableDefault(size = 180) Pageable pageable) {
+        return ResponseEntity.ok(stockService.getCandlePriceHistoryByCode(pageable, code));
     }
 }
