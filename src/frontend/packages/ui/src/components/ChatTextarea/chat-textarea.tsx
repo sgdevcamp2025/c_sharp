cc72bb55ba6e2e22d45a5d4d659a13bcfda06929
KimKyuHoi <k546kh@gmail.com>
2025-01-29 23:59:43 +0900

    #66 feat(fe): textarea 컴포넌트 내용 추가

--- src/frontend/packages/ui/src/components/ChatTextarea/chat-textarea.tsx
+++ src/frontend/packages/ui/src/components/ChatTextarea/chat-textarea.tsx
@@ -7,7 +7,7 @@ import { cn } from '@workspace/ui/lib/utils';
 
 const ChatTextarea = ({ onSend, onAdd }) => {
   return (
-    <div className={cn('flex flex-col items-center w-full rounded-md border border-gray-300 px-0.5 py-1')}>
+    <div className={cn('flex flex-col items-center w-full rounded-md border bg-white border-gray-300 px-0.5 py-1')}>
       <Textarea placeholder="Type your message..." />
       <div className="flex justify-between w-full px-1">
         <Button
