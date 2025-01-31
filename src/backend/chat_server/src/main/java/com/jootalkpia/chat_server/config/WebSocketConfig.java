8961550c625d98ef73772bb19e373928b063928f
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-01 02:54:41 +0900

    #80 feat(be): 채팅서버 환경셋팅 - websocket 관련 설정 추가

---
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.jootalkpia.chat_server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        //스프링 인메모리 메시지 브로커 사용
+        config.enableSimpleBroker("/subscribe");    //구독 prefix
+        config.setApplicationDestinationPrefixes("/publish");   //발행 prefix
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-connect") //handShake과정에서 사용할 endpoint
+                .setAllowedOriginPatterns("*") // cors
+                .withSockJS(); // SockJS 활성화
+    }
+}
