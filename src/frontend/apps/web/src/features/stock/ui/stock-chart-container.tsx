4ba2578cbcdeab668ec32b7329c64eb7512216f9
Sonjieun <sonjieun2661@gmail.com>
2025-02-02 23:49:26 +0900

    #101 feat(fe): 주식차트 레이아웃 분리 및 스타일링

--- src/frontend/apps/web/src/features/stock/ui/stock-chart-container.tsx
+++ src/frontend/apps/web/src/features/stock/ui/stock-chart-container.tsx
@@ -1,7 +1,27 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@workspace/ui/components';
+
 const StockChartContainer = () => {
   return (
     <>
-      <h1>StockChartContainer Component</h1>
+      <ResizablePanelGroup direction="vertical">
+        <ResizablePanel
+          defaultSize={30}
+          className="flex items-center justify-center bg-gray-200 p-4"
+        >
+          주식차트
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel
+          defaultSize={10}
+          className="flex items-center justify-center bg-gray-300 p-4"
+        >
+          거래량 캔들차트
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </>
   );
 };
--- src/frontend/apps/web/src/features/stock/ui/stock-detail-layout.tsx
+++ src/frontend/apps/web/src/features/stock/ui/stock-detail-layout.tsx
@@ -3,11 +3,11 @@ import StockInfoContainer from './stock-info-container';
 
 const StockDetailLayout = () => {
   return (
-    <div className="bg-gray-200 w-full h-full flex flex-col gap-2">
+    <div className="w-full h-full flex flex-col gap-2">
       <div className="h-1/5 bg-red-400">
         <StockInfoContainer />
       </div>
-      <div className="h-full bg-blue-400">
+      <div className="h-full rounded-md overflow-hidden">
         <StockChartContainer />
       </div>
     </div>
