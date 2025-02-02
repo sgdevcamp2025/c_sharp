59f9361cbcebbf4010a046e04a73509bcf8fbffa
Sonjieun <sonjieun2661@gmail.com>
2025-02-03 00:13:46 +0900

    #101 feat(fe): 주식정보 상세 정보 구현

--- src/frontend/apps/web/src/features/stock/ui/stock-logo.tsx
+++ src/frontend/apps/web/src/features/stock/ui/stock-logo.tsx
@@ -1,7 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@workspace/ui/components';
+
 const StockLogo = () => {
   return (
     <>
-      <h1>StockLogo Component</h1>
+      <Avatar
+        variant="square"
+        className="w-[60px] h-[60px]"
+      >
+        <AvatarImage src="https://thumb.tossinvest.com/image/resized/48x0/https%3A%2F%2Fstatic.toss.im%2Fpng-icons%2Fsecurities%2Ficn-sec-fill-NAS006XY7-E0.png" />
+        <AvatarFallback>Logo</AvatarFallback>
+      </Avatar>
     </>
   );
 };
