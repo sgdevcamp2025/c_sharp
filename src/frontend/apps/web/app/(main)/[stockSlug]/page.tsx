9c161aac36e66f65cf169f44ee9750036e35163e
Sonjieun <sonjieun2661@gmail.com>
2025-02-03 00:49:03 +0900

    #102 fix(fe):상세페이지 불필요한 스타일링 삭제

--- src/frontend/apps/web/app/(main)/[stockSlug]/page.tsx
+++ src/frontend/apps/web/app/(main)/[stockSlug]/page.tsx
@@ -20,7 +20,7 @@ export default function StockDetailsPage({ params }) {
   // console.log(1, params);
 
   return (
-    <div className="flex bg-gray-700 py-6 px-[30px] h-full">
+    <div className="flex py-6 px-[30px] h-full">
       <div className="pr-2 w-[45vw] flex flex-col">
         <Link
           href={'/'}
