6ff7a597648ce5d0408d8202b66f857abf4e7089
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-02 18:12:51 +0900

    #82 feat(be): 히스토리 서버 환경셋팅

---
+++ src/backend/history_server/src/main/java/com/jootalkpia/history_server/HistoryServerApplication.java
@@ -0,0 +1,13 @@
+package com.jootalkpia.history_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryServerApplication.class, args);
+	}
+
+}
