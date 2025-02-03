316ea5477d9f052a94f12e2d2ed44e8b8dc1d06f
Sonjieun <sonjieun2661@gmail.com>
2025-02-03 20:36:36 +0900

    #111 feat(fe):사이드바 기본 레이아웃

---
+++ src/frontend/apps/web/src/shared/components/sidebar/ui/sidebar-container.tsx
@@ -0,0 +1,93 @@
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@workspace/ui/components';
+import { ChevronRight } from 'lucide-react';
+
+const SidebarContainer = () => {
+  const data = {
+    navMain: [
+      {
+        title: '참여한 채널',
+        items: [
+          {
+            title: 'one',
+            url: '#',
+          },
+          {
+            title: 'two',
+            url: '#',
+            isActive: true,
+          },
+        ],
+      },
+      {
+        title: '전체 채널',
+        items: [
+          {
+            title: 'three',
+            url: '#',
+          },
+          {
+            title: 'four',
+            url: '#',
+          },
+        ],
+      },
+    ],
+  };
+  return (
+    <Sidebar>
+      <SidebarHeader>#WSName</SidebarHeader>
+      <SidebarContent className="gap-0">
+        {data.navMain.map((item) => (
+          <Collapsible
+            key={item.title}
+            title={item.title}
+            defaultOpen
+            className="group/collapsible"
+          >
+            <SidebarGroup>
+              <SidebarGroupLabel
+                asChild
+                className="group/label text-sm text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground"
+              >
+                <CollapsibleTrigger>
+                  {item.title}{' '}
+                  <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                </CollapsibleTrigger>
+              </SidebarGroupLabel>
+              <CollapsibleContent>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    {item.items.map((item) => (
+                      <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton
+                          asChild
+                          isActive={item.isActive}
+                        >
+                          <a href={item.url}>{item.title}</a>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    ))}
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+        ))}
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+export default SidebarContainer;
