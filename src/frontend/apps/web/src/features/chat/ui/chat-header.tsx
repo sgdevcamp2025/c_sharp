e1bada448197fd01ce15763252afcd31b63cb0b8
Sonjieun <sonjieun2661@gmail.com>
2025-02-03 21:27:10 +0900

    #111 feat(fe): 사이드바 trigger 배치

--- src/frontend/apps/web/src/features/chat/ui/chat-header.tsx
+++ src/frontend/apps/web/src/features/chat/ui/chat-header.tsx
@@ -1,5 +1,12 @@
+import { SidebarTrigger } from '@workspace/ui/components';
+
 const ChatHeader = () => {
-  return <header className="h-[54px] w-full flex items-center justify-between px-2.5 py-[13px] bg-slate-300">123</header>;
+  return (
+    <header className="h-[54px] w-full flex items-center justify-between px-2.5 py-[13px] bg-slate-300">
+      <SidebarTrigger />
+      123
+    </header>
+  );
 };
 
 export default ChatHeader;
