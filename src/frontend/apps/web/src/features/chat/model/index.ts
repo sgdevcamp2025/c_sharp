15c3f10dc326bb600145a192c6988dcbc72a13dc
KimKyuHoi <k546kh@gmail.com>
2025-02-03 21:09:09 +0900

    #110 feat(fe): code 모듈화

--- src/frontend/apps/web/src/features/chat/model/index.ts
+++ src/frontend/apps/web/src/features/chat/model/index.ts
@@ -1,3 +1,7 @@
 export type { FileData } from './file-data.type';
-export { validateFileSize, generateVideoThumbnail } from './file.utils';
+export {
+  validateFileSize,
+  generateVideoThumbnail,
+  formatFileSize,
+} from './file.utils';
 export { handleFileChangeEvent, removeFile } from './file-upload.util';
