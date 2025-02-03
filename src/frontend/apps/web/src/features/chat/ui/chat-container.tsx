999627f2016994b9d8757359c455080cd63ce6ca
Sonjieun <sonjieun2661@gmail.com>
2025-02-03 21:40:24 +0900

    #111 feat(fe): 사이드바 border설정

--- src/frontend/apps/web/src/features/chat/ui/chat-container.tsx
+++ src/frontend/apps/web/src/features/chat/ui/chat-container.tsx
@@ -1,15 +1,11 @@
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@workspace/ui/components';
+import { SidebarInset, SidebarProvider } from '@workspace/ui/components';
 import ChatHeader from './chat-header';
 import ChatSection from './chat-section';
 import { SidebarContainer } from '@/src/shared/components/sidebar';
 
 const ChatContainer = () => {
   return (
-    <SidebarProvider className="flex w-full h-full min-w-0 min-h-0 bg-white">
+    <SidebarProvider className="flex w-full h-full min-w-0 min-h-0 border  rounded-md overflow-hidden">
       <SidebarContainer />
       <SidebarInset className="flex flex-col min-w-0 min-h-0 w-full h-full">
         <ChatHeader />
