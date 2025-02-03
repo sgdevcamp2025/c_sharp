7e5e83949a7908f266dadd31f3c2dd64e0693c4a
KimKyuHoi <k546kh@gmail.com>
2025-02-04 03:10:21 +0900

    #110 refactor(fe): 커스텀 훅 컴포넌트 수정

--- src/frontend/apps/web/src/features/chat/model/index.ts
+++ src/frontend/apps/web/src/features/chat/model/index.ts
@@ -1,7 +1,2 @@
 export type { FileData } from './file-data.type';
-export {
-  validateFileSize,
-  generateVideoThumbnail,
-  formatFileSize,
-} from './file.utils';
-export { handleFileChangeEvent, removeFile } from './file-upload.util';
+export { useFileManagements } from './use-file-managements';
