7997ccc3fcc74c5e05e2ed63b383bcb4eb25ee7a
KimSehoon <shggm2000@gmail.com>
2025-02-04 16:05:11 +0900

    #118 refactor(be): processMinutePrice method

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/service/KafkaConsumer.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/service/KafkaConsumer.java
@@ -1,31 +1,32 @@
 package com.jootalkpia.chat_server.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jootalkpia.chat_server.dto.ChatMessageToKafka;
 import com.jootalkpia.chat_server.dto.MinutePriceResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class KafkaConsumer {
     @KafkaListener(
             topics = "jootalkpia.stock.local.minute",
-            groupId = "minute-price-save-consumer-group",
-            concurrency = "2"
+            groupId = "minute-price-save-consumer-group"
     )
-    public void processMinutePriceMessage(String kafkaMessage) {
+    public void processMinutePrice(String kafkaMessage) {
         log.info("message ===> " + kafkaMessage);
 
         ObjectMapper mapper = new ObjectMapper();
 
         try {
             MinutePriceResponse minutePriceResponse = mapper.readValue(kafkaMessage, MinutePriceResponse.class);
+
+            //웹소켓 전달하는 로직 or 전달하는 함수
+
             log.info("dto ===> " + minutePriceResponse.toString());
         } catch (Exception ex) {
-            log.error(ex.getMessage(), ex);
+            log.error(ex.getMessage(), ex); // 추후에 GlobalException 처리
         }
     }
 }
