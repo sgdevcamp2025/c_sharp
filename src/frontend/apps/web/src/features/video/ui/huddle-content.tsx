406fdb12041ad365cd75a470a3a87a57a07331d2
Sonjieun <sonjieun2661@gmail.com>
2025-02-04 23:03:51 +0900

    #112 fix(fe): 참여자 아이콘 스타일링 수정

--- src/frontend/apps/web/src/features/video/ui/huddle-content.tsx
+++ src/frontend/apps/web/src/features/video/ui/huddle-content.tsx
@@ -2,11 +2,11 @@ import HuddleParticipant from './huddle-participant';
 
 const HuddleContent = () => {
   return (
-    <div className="w-full h-full min-h-0 overflow-hidden p-3 bg-muted rounded-lg flex flex-row flex-wrap justify-around items-center gap-1 overflow-y-auto">
+    <div className="w-full h-full min-h-0 overflow-hidden p-3 bg-muted rounded-lg flex flex-row flex-wrap justify-center items-center gap-5 overflow-y-auto">
       <HuddleParticipant />
       <HuddleParticipant />
-      <HuddleParticipant />
-      <HuddleParticipant />
+      {/* <HuddleParticipant />
+      <HuddleParticipant /> */}
     </div>
   );
 };
--- src/frontend/apps/web/src/features/video/ui/huddle-participant.tsx
+++ src/frontend/apps/web/src/features/video/ui/huddle-participant.tsx
@@ -2,7 +2,7 @@ import HuddleParticipantAvatar from './huddle-participant-avatar';
 
 const HuddleParticipant = () => {
   return (
-    <div className="h-1/2 aspect-square rounded-md overflow-hidden">
+    <div className="h-[45%] aspect-square rounded-md overflow-hidden">
       <HuddleParticipantAvatar />
     </div>
   );
