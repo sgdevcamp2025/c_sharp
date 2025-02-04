86b28f408bf72817d1064b68e1cad2b094048d14
Sonjieun <sonjieun2661@gmail.com>
2025-02-04 11:29:53 +0900

    #100 fix(fe): toggle-group action 에러픽스

--- src/frontend/packages/ui/src/components/Toggle/toggle-group.stories.tsx
+++ src/frontend/packages/ui/src/components/Toggle/toggle-group.stories.tsx
@@ -1,7 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ToggleGroup, ToggleGroupItem } from './toggle-group';
 import { useState } from 'react';
-import { Mic, MicOff, ScreenShare, ScreenShareOff, Video, VideoOff } from 'lucide-react';
+import {
+  Mic,
+  MicOff,
+  ScreenShare,
+  ScreenShareOff,
+  Video,
+  VideoOff,
+} from 'lucide-react';
 
 const meta: Meta<typeof ToggleGroup> = {
   title: 'Widget/ToggleGroup',
@@ -14,37 +21,46 @@ const meta: Meta<typeof ToggleGroup> = {
 export default meta;
 type Story = StoryObj<typeof ToggleGroup>;
 
-export const Default: Story = {
-  render: () => {
-    const [isMicOn, setIsMicOn] = useState(false);
-    const [isVideoOn, setIsVideoOn] = useState(false);
-    const [isScreenSharing, setIsScreenSharing] = useState(false);
+const ToolBar = ({
+  type = 'multiple',
+  variant = 'outline',
+  size = 'lg',
+}: {
+  type?: 'multiple';
+  variant?: 'default' | 'outline';
+  size?: 'default' | 'lg';
+}) => {
+  const [isMicOn, setIsMicOn] = useState(false);
+  const [isVideoOn, setIsVideoOn] = useState(false);
+  const [isScreenSharing, setIsScreenSharing] = useState(false);
 
-    return (
-      <ToggleGroup
-        variant="outline"
-        type="multiple"
-        size="lg"
+  return (
+    <ToggleGroup
+      type={type}
+      variant={variant}
+      size={size}
+    >
+      <ToggleGroupItem
+        value="mic"
+        onClick={() => setIsMicOn((prev) => !prev)}
       >
-        <ToggleGroupItem
-          value="mic"
-          onClick={() => setIsMicOn((prev) => !prev)}
-        >
-          {isMicOn ? <Mic /> : <MicOff />}
-        </ToggleGroupItem>
-        <ToggleGroupItem
-          value="video"
-          onClick={() => setIsVideoOn((prev) => !prev)}
-        >
-          {isVideoOn ? <Video /> : <VideoOff />}
-        </ToggleGroupItem>
-        <ToggleGroupItem
-          value="sharing"
-          onClick={() => setIsScreenSharing((prev) => !prev)}
-        >
-          {isScreenSharing ? <ScreenShare /> : <ScreenShareOff />}
-        </ToggleGroupItem>
-      </ToggleGroup>
-    );
-  },
+        {isMicOn ? <Mic /> : <MicOff />}
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="video"
+        onClick={() => setIsVideoOn((prev) => !prev)}
+      >
+        {isVideoOn ? <Video /> : <VideoOff />}
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="sharing"
+        onClick={() => setIsScreenSharing((prev) => !prev)}
+      >
+        {isScreenSharing ? <ScreenShare /> : <ScreenShareOff />}
+      </ToggleGroupItem>
+    </ToggleGroup>
+  );
+};
+export const Default: Story = {
+  render: () => <ToolBar />,
 };
