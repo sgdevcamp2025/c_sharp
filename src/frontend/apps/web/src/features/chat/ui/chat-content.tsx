8007e603d4871856c9c957e4be42248092478512
KimKyuHoi <k546kh@gmail.com>
2025-02-04 16:05:09 +0900

    #71 feat(fe): chatting 컴포넌트식 호버할 시 보이도록 구현

--- src/frontend/apps/web/src/features/chat/ui/chat-content.tsx
+++ src/frontend/apps/web/src/features/chat/ui/chat-content.tsx
@@ -1,6 +1,8 @@
 import ContentText from './content-text';
 import ContentAvatar from './content-avatar';
 
+import { MessageSquareText } from 'lucide-react';
+
 export type ChatContentProps = {
   type?: 'default' | 'live';
 };
@@ -23,11 +25,20 @@ const ChatContent = ({
       className={`flex w-full h-auto ${backgroundColor} pb-2 pl-5 pt-5 pr-6 gap-4 group relative ${hoverColor} transition-all duration-300`}
     >
       <ContentAvatar type={type} />
-      <ContentText
-        type={type}
-        avatarUrls={avatarUrls}
-        setIsThreadOpen={setIsThreadOpen}
-      />
+      <div className="flex w-full items-start justify-between">
+        <ContentText
+          type={type}
+          avatarUrls={avatarUrls}
+          setIsThreadOpen={setIsThreadOpen}
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+          <MessageSquareText
+            size="15"
+            className="cursor-pointer hover:text-gray-600"
+            onClick={() => setIsThreadOpen(true)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
