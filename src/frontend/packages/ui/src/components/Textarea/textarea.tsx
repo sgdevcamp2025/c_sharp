679980ebc37da808da6ba08c2654cb10aedc015a
KimKyuHoi <k546kh@gmail.com>
2025-02-05 16:23:22 +0900

    #141 fix(fe): textarea resizeble 막기 및 css 수정

--- src/frontend/packages/ui/src/components/Textarea/textarea.tsx
+++ src/frontend/packages/ui/src/components/Textarea/textarea.tsx
@@ -21,7 +21,7 @@ const Textarea = React.forwardRef<
   return (
     <textarea
       className={cn(
-        'flex min-h-[64px] h-auto w-full rounded-md bg-white px-3 py-2 text-base placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        'flex min-h-[64px] max-h-[128px] resize-none h-auto w-full rounded-md bg-white px-3 py-2 text-base placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
         className,
       )}
       maxLength={2000}
