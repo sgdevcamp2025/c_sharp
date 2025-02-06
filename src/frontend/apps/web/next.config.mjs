3e35d4da812ff154641f07c28a6414b45e4a8bf4
KimKyuHoi <k546kh@gmail.com>
2025-02-06 14:06:51 +0900

    #144 fix(fe): netlify 배포 수정

--- src/frontend/apps/web/next.config.mjs
+++ src/frontend/apps/web/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   transpilePackages: ['@workspace/ui'],
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
