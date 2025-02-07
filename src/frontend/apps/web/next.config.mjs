48f59e1508abd5e49c7287b9e9eda3150be29ef8
KimKyuHoi <k546kh@gmail.com>
2025-02-07 16:11:58 +0900

    #144 fix(fe): next 세부설정

--- src/frontend/apps/web/next.config.mjs
+++ src/frontend/apps/web/next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
   output: 'standalone', // standalone 출력 추가
   transpilePackages: ['@workspace/ui'],
-  // Netlify에서의 정적 에셋 처리를 위한 설정
-  images: {
-    unoptimized: true,
+  trailingSlash: true, // 경로 문제 해결 (next export 시 필요)
+  experimental: {
+    outputStandalone: true,
   },
 };
 
