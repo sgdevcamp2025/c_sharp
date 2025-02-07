5eadfbf47f15d85a61bca65ff7d33359dd9749ad
KimKyuHoi <k546kh@gmail.com>
2025-02-07 16:26:01 +0900

    #144 fix(fe): standalone 모드 사용

--- src/frontend/apps/web/next.config.mjs
+++ src/frontend/apps/web/next.config.mjs
@@ -2,10 +2,6 @@
 const nextConfig = {
   output: 'standalone', // standalone 출력 추가
   transpilePackages: ['@workspace/ui'],
-  trailingSlash: true, // 경로 문제 해결 (next export 시 필요)
-  experimental: {
-    outputStandalone: true,
-  },
 };
 
 export default nextConfig;
