046f6a4743a6d69575820a3a8217654c32b69e82
KimSehoon <shggm2000@gmail.com>
2025-02-07 19:59:34 +0900

    #116 feat(be): findByCodeAndMinutePriceIdGreaterThanOrderByMinutePriceIdAsc method 구현
    - findByCodeAndMinutePriceIdGreaterThanOrderByMinutePriceIdAsc는 클라이언트가 요청한 마지막 ObjectId부터 size만큼의 데이터를 가져옴

--- src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/repository/MinutePriceRepository.java
+++ src/backend/stock_server/src/main/java/com/jootalkpia/stock_server/stocks/repository/MinutePriceRepository.java
@@ -1,6 +1,7 @@
 package com.jootalkpia.stock_server.stocks.repository;
 
 import com.jootalkpia.stock_server.stocks.dto.MinutePrice;
+import org.bson.types.ObjectId;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -9,6 +10,12 @@ import java.util.List;
 public interface MinutePriceRepository extends MongoRepository<MinutePrice, String> {
     List<MinutePrice> findByCodeOrderByMinutePriceIdAsc(
             String code,
+            Pageable pageable
+    );
+
+    List<MinutePrice> findByCodeAndMinutePriceIdGreaterThanOrderByMinutePriceIdAsc(
+            String code,
+            ObjectId minutePriceId,
             Pageable pageable
     );
 }
