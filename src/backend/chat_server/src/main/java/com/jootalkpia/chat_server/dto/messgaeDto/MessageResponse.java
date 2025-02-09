bf377d17655e883f96073adf4e589a4c74038cbc
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-10 03:57:12 +0900

    #81 feat(be): kafka내 Json처리를 위한 interface type 명시

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/dto/messgaeDto/MessageResponse.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/dto/messgaeDto/MessageResponse.java
@@ -1,5 +1,19 @@
 package com.jootalkpia.chat_server.dto.messgaeDto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = CommonResponse.class, name = "COMMON"),
+        @JsonSubTypes.Type(value = TextResponse.class, name = "TEXT"),
+        @JsonSubTypes.Type(value = VideoResponse.class, name = "VIDEO"),
+        @JsonSubTypes.Type(value = ImageResponse.class, name = "IMAGE")
+})
 public interface MessageResponse {
     String type();
 }
