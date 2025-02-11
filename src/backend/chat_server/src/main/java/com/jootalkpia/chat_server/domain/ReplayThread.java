b8b3c927192c3813f5d47330e89a876f0e21abdf
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-11 11:33:20 +0900

    #171 feat(be): ReplayThread 엔티티 구현

---
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/domain/ReplayThread.java
@@ -0,0 +1,19 @@
+package com.jootalkpia.chat_server.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Table(name = "ReplayThreads")
+@Getter
+public class ReplayThread extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long RelayThreadId;
+
+    private Long ThreadId;
+}
