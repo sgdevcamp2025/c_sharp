6508c43daef254d25fd3c578a0737a32412a6e91
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-11 16:44:45 +0900

    #171 feat(be): commonResponse에 thread 및 channel id 정보 추가

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/dto/messgaeDto/CommonResponse.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/dto/messgaeDto/CommonResponse.java
@@ -1,6 +1,8 @@
 package com.jootalkpia.chat_server.dto.messgaeDto;
 
 public record CommonResponse(
+        Long channelId,
+        Long threadId,
         Long userId,
         String userNickname,
         String userProfileImage
