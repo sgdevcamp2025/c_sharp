6e6708e6a0d1d20d71a441ad52c2d3676b44dd25
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-14 01:40:54 +0900

    #193 fix(be): 공통 응답에 메세지 시간 추가 - dto

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/dto/messgaeDto/CommonResponse.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/dto/messgaeDto/CommonResponse.java
@@ -3,6 +3,7 @@ package com.jootalkpia.chat_server.dto.messgaeDto;
 public record CommonResponse(
         Long channelId,
         Long threadId,
+        String threadDateTime,
         Long userId,
         String userNickname,
         String userProfileImage
