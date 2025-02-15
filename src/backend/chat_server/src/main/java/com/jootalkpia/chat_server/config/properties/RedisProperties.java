8f134f7713a888171c58b0a9aac6af6197318fa2
KimSehoon <shggm2000@gmail.com>
2025-02-15 18:50:35 +0900

    #197 feat(be): ConfigurationProperties 구현

---
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/config/properties/RedisProperties.java
@@ -0,0 +1,10 @@
+package com.jootalkpia.chat_server.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.data.redis")
+public record RedisProperties(
+        String host,
+        int port
+) {
+}
