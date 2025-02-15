c5ba22dbfb4057798f684e7054aa8ec0be679a05
Sonjieun <sonjieun2661@gmail.com>
2025-02-15 21:45:35 +0900

    #188 fix(fe): stock layout 중첩에러 해결

--- src/frontend/apps/web/app/stock/layout.tsx
+++ src/frontend/apps/web/app/stock/layout.tsx
@@ -9,18 +9,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-    >
-      <body>
-        <header className="h-[68px] w-full flex items-center justify-between px-4 py-1 bg-muted">
-          <Header authContent={<ProfilePopover />} />
-        </header>
-        <div className="flex flex-col h-[calc(100vh-68px)] overflow-hidden">
-          <AuthWrapper> {children}</AuthWrapper>
-        </div>
-      </body>
-    </html>
+    <>
+      <header className="h-[68px] w-full flex items-center justify-between px-4 py-1 bg-muted">
+        <Header authContent={<ProfilePopover />} />
+      </header>
+      <div className="flex flex-col h-[calc(100vh-68px)] overflow-hidden">
+        <AuthWrapper> {children}</AuthWrapper>
+      </div>
+    </>
   );
 }
