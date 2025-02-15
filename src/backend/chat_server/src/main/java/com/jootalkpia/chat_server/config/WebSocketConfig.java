99ebbe2d81f7b287e3e90789e9f48bae33058d6b
KimSehoon <shggm2000@gmail.com>
2025-02-15 18:53:56 +0900

    #197 feat(be): configureClientInboundChannel 구현
    - stompSubscriptionInterceptor 의존성 추가

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/config/WebSocketConfig.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.jootalkpia.chat_server.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +10,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompSubscriptionInterceptor stompSubscriptionInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -20,5 +25,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-connect") // STOMP WebSocket 엔드포인트
                 .setAllowedOriginPatterns("*") // CORS 허용
                 .withSockJS(); // SockJS 활성화
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompSubscriptionInterceptor);
     }
 }
