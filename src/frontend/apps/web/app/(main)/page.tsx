2019e1791d61567136c4ce91d6eb69890288d1d6
Sonjieun <sonjieun2661@gmail.com>
2025-02-15 20:44:44 +0900

    #188 feat(fe): 로그인페이지 생성

--- src/frontend/apps/web/app/(main)/page.tsx
+++ src/frontend/apps/web/app/(main)/page.tsx
@@ -1,12 +1,32 @@
-import { Button } from '@workspace/ui/components';
-import Link from 'next/link';
+import { LoginButton } from '@/src/features/auth';
+
+export const metadata = {
+  title: '주톡피아 - 카카오 로그인',
+  description:
+    '주톡피아에서 카카오 로그인을 통해 간편하게 시작하세요. 실시간 주식 정보와 협업 기능을 한곳에서 제공하여, 투자와 소통',
+  openGraph: {
+    title: '주톡피아',
+    description:
+      '주톡피아에서 카카오 로그인을 통해 간편하게 시작하세요. 실시간 주식 정보와 협업 기능을 한곳에서 제공하여, 투자와 소통',
+    url: '',
+    siteName: '주톡피아',
+    images: [
+      {
+        url: '',
+        width: 1200,
+        height: 630,
+        alt: '주톡피아',
+      },
+    ],
+    type: 'website',
+  },
+};
 
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>
-        <Link href="/stock">로그인버튼</Link>
-      </Button>
+      <h1 className="text-3xl text-primary font-bold">주톡피아</h1>
+      <LoginButton />
     </div>
   );
 }
