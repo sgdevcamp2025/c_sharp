082803e67bb2a6805999b946bf00f8fa84a664b5
KimKyuHoi <k546kh@gmail.com>
2025-02-16 04:10:33 +0900

    #67 fix: stock container 수정

--- src/frontend/apps/web/src/features/stock/ui/stock-chart-container.tsx
+++ src/frontend/apps/web/src/features/stock/ui/stock-chart-container.tsx
@@ -8,14 +8,14 @@ const StockChartContainer = () => {
   return (
     <ResizablePanelGroup direction="vertical">
       <ResizablePanel
-        defaultSize={30}
+        defaultSize={70}
         className="flex items-center justify-center bg-gray-200 p-4"
       >
         주식차트
       </ResizablePanel>
       <ResizableHandle withHandle />
       <ResizablePanel
-        defaultSize={10}
+        defaultSize={30}
         className="flex items-center justify-center bg-gray-300 p-4"
       >
         거래량 캔들차트
