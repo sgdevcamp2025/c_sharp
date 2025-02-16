4d098a860aa5bc08fa06bfc15062912e84330bab
KimSehoon <shggm2000@gmail.com>
2025-02-17 02:07:16 +0900

    #207 feat(be): RedisConfig

---
+++ src/backend/workspace_server/src/main/java/com/jootalkpia/workspace_server/util/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.jootalkpia.workspace_server.util.config;
+
+import com.jootalkpia.workspace_server.util.property.RedisProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.host(), redisProperties.port());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        try {
+            template.getConnectionFactory().getConnection().ping();
+            log.info("Successfully connected to Redis");
+        } catch (Exception e) {
+            log.error("Failed to connect to Redis", e);
+        }
+
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setValueSerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+        template.setHashValueSerializer(stringSerializer);
+
+        return template;
+    }
+}
