87975cb82f002ae93582fd4e516d7f333221802e
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-17 01:13:13 +0900

    #125 feat(be) : auth서버에 common module scan 등록

--- src/backend/auth_server/src/main/java/com/jootalkpia/auth_server/AuthServerApplication.java
+++ src/backend/auth_server/src/main/java/com/jootalkpia/auth_server/AuthServerApplication.java
@@ -5,12 +5,18 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @EnableFeignClients
-@SpringBootApplication
 @ImportAutoConfiguration(FeignAutoConfiguration.class)
+@SpringBootApplication(scanBasePackages = {
+        "com.jootalkpia.auth_server",
+        "com.jootalkpia.passport",
+        "com.jootalkpia.aop",
+        "com.jootalkpia.config",
+})
 public class AuthServerApplication {
 
     public static void main(String[] args) {
