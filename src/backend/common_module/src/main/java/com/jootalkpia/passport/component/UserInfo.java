b8a60538325694ba50c00bfdec741913d58eaa35
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-17 17:07:44 +0900

    #125 fix(be) : passport에 담는 userInfo 객체 수정

--- src/backend/common_module/src/main/java/com/jootalkpia/passport/component/UserInfo.java
+++ src/backend/common_module/src/main/java/com/jootalkpia/passport/component/UserInfo.java
@@ -1,11 +1,6 @@
 package com.jootalkpia.passport.component;
 
 public record UserInfo(
-        Long userId,
-        String nickname,
-        Boolean isActivated,
-        String accessedAt,
-        String createdAt,
-        String deletedAt
+        Long userId
 ) {
 }
