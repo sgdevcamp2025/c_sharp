4c3b9b61e8e8e11904b9d6943e393aaa68efe3c0
KimSehoon <shggm2000@gmail.com>
2025-02-18 15:05:13 +0900

    #221 style(be): repository 패키지 이동

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/repository/jpa/ThreadRepository.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/repository/jpa/ThreadRepository.java
@@ -1,4 +1,4 @@
-package com.jootalkpia.chat_server.repository;
+package com.jootalkpia.chat_server.repository.jpa;
 
 import com.jootalkpia.chat_server.domain.Thread;
 import org.springframework.data.jpa.repository.JpaRepository;
