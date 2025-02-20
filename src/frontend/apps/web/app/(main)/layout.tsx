954d25a5e80addffde2c66771c017a7f97709515
Sonjieun <sonjieun2661@gmail.com>
2025-02-20 23:31:29 +0900

    #174 fix(fe): 로그인페이지 레이아웃 수정 및 불필요한 코드 삭제

--- src/frontend/apps/web/app/(main)/layout.tsx
+++ src/frontend/apps/web/app/(main)/layout.tsx
@@ -1,7 +1,4 @@
-import { LoginButton } from '@/src/features/auth';
 import AuthWrapper from '@/src/features/auth/ui/auth-wrapper';
-import { ProfilePopover } from '@/src/features/user';
-import { Header } from '@/src/shared';
 import '@workspace/ui/globals.css';
 
 export default function RootLayout({
@@ -9,17 +6,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = null;
   return (
     <html
       lang="en"
       suppressHydrationWarning
     >
       <body>
-        <header className="h-[68px] w-full flex items-center justify-between px-4 py-1 bg-muted">
-          <Header authContent={user ? <ProfilePopover /> : <LoginButton />} />
-        </header>
-        <div className="flex flex-col h-[calc(100vh-68px)] overflow-hidden">
+        <div className="flex flex-col overflow-hidden bg-muted">
           <AuthWrapper>{children}</AuthWrapper>
         </div>
       </body>
