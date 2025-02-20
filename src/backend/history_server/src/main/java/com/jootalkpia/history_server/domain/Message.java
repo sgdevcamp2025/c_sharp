8be9f77fe5b4c1aa7ed3b3cdb8758d2ef7818cb1
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-20 16:08:04 +0900

    #225 fix(be): message 조회 시 null 값 제거

--- src/backend/history_server/src/main/java/com/jootalkpia/history_server/domain/Message.java
+++ src/backend/history_server/src/main/java/com/jootalkpia/history_server/domain/Message.java
@@ -1,10 +1,12 @@
 package com.jootalkpia.history_server.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Message {
     @Field("type")
     private String type;
