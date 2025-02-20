db5c10debab0c9f9204e357fdbc94982698b131e
이미르 <91879211+mirlee0304@users.noreply.github.com>
2025-02-20 09:37:32 +0900

    #245 feat(be): stomp 연결 수정

--- src/backend/signaling_server/src/main/java/com/jootalkpia/signaling_server/SignalingServerApplication.java
+++ src/backend/signaling_server/src/main/java/com/jootalkpia/signaling_server/SignalingServerApplication.java
@@ -3,7 +3,7 @@ package com.jootalkpia.signaling_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.jootalkpia.signaling_server")
 public class SignalingServerApplication {
 
     public static void main(String[] args) {
