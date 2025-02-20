4e8425242d03ecc4ab8db9dd2083685f90393ceb
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-20 11:36:26 +0900

    #225 feat(be): 과거 채팅 조회 api 구현 - repository

--- src/backend/history_server/src/main/java/com/jootalkpia/history_server/repository/ChatMessageRepository.java
+++ src/backend/history_server/src/main/java/com/jootalkpia/history_server/repository/ChatMessageRepository.java
@@ -1,7 +1,18 @@
 package com.jootalkpia.history_server.repository;
 
 import com.jootalkpia.history_server.domain.ChatMessage;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ChatMessageRepository extends MongoRepository<ChatMessage,String> {
+import java.util.List;
+
+@Repository
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+
+    // 첫 페이지 요청 (cursorId가 없는 경우)
+    List<ChatMessage> findByChannelIdOrderByThreadIdAsc(Long channelId, Pageable pageable);
+
+    // cursorId 이후의 메시지 조회 (페이징)
+    List<ChatMessage> findByChannelIdAndThreadIdGreaterThanOrderByThreadIdAsc(Long channelId, Long threadId, Pageable pageable);
 }
