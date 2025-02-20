3166ae626de4e65f50f93a6c1101cc78daee0568
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-20 22:06:42 +0900

    #226 feat(be): 역방향 과거 채팅 조회 api 구현 - repository

--- src/backend/history_server/src/main/java/com/jootalkpia/history_server/repository/ChatMessageRepository.java
+++ src/backend/history_server/src/main/java/com/jootalkpia/history_server/repository/ChatMessageRepository.java
@@ -1,6 +1,7 @@
 package com.jootalkpia.history_server.repository;
 
 import com.jootalkpia.history_server.domain.ChatMessage;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,10 @@ import java.util.List;
 
 @Repository
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
-    // cursorId 이후의 메시지 조회 (페이징)
+
+    // cursorId 이후의 메시지 조회 (정방향)
     List<ChatMessage> findByChannelIdAndThreadIdGreaterThanOrderByThreadIdAsc(Long channelId, Long threadId, Pageable pageable);
+
+    // 역방향 조회 (lastReadId 이하의 메시지)
+    List<ChatMessage> findByChannelIdAndThreadIdLessThanEqualOrderByThreadIdDesc(Long channelId, Long threadId, Pageable pageable);
 }
