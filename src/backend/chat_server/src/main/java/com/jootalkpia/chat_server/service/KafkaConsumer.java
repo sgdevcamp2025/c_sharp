a625c9c509bd3b0dfd55bf2038ecba8f7c07493b
KimSehoon <shggm2000@gmail.com>
2025-02-20 22:17:04 +0900

    #256 feat(be): processPushMessage 구현

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/service/KafkaConsumer.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/service/KafkaConsumer.java
@@ -1,8 +1,10 @@
 package com.jootalkpia.chat_server.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jootalkpia.chat_server.dto.ChatMessageToKafka;
 import com.jootalkpia.chat_server.dto.MinutePriceResponse;
+import com.jootalkpia.chat_server.dto.PushMessageToKafka;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -25,6 +27,7 @@ public class KafkaConsumer {
     )
     public void processMinutePrice(String kafkaMessage) {
         log.info("Received Kafka minute message ===> {}", kafkaMessage);
+
         try {
             MinutePriceResponse stockUpdate = objectMapper.readValue(kafkaMessage, MinutePriceResponse.class);
             String stockDataJson = objectMapper.writeValueAsString(stockUpdate);
@@ -45,15 +48,36 @@ public class KafkaConsumer {
     )
     public void processChatMessage(@Header(KafkaHeaders.RECEIVED_KEY) String channelId, String kafkaMessage) {
         log.info("Received Kafka message ===> channelId: {}, message: {}", channelId, kafkaMessage);
+
         try {
             ChatMessageToKafka chatMessage = objectMapper.readValue(kafkaMessage, ChatMessageToKafka.class);
             String chatDataJson = objectMapper.writeValueAsString(chatMessage);
 
             messagingTemplate.convertAndSend("/subscribe/chat." + channelId, chatDataJson);
             log.info("Broadcasted chat message via WebSocket: {}", chatDataJson);
+            log.info("/subscribe/chat." + channelId);
 
         } catch (Exception ex) {
             log.error("Error processing chat message: {}", ex.getMessage(), ex);
+        }
+    }
+
+    @KafkaListener(
+            topics = "${topic.push}",
+            groupId = "${group.push}"
+    )
+    public void processPushMessage(String kafkaMessage) {
+        log.info("message ===> " + kafkaMessage);
+
+        try {
+            PushMessageToKafka pushMessageToKafka = objectMapper.readValue(kafkaMessage, PushMessageToKafka.class);
+            String cleanSessionId = pushMessageToKafka.sessionId().replace("\"", "");
+
+            messagingTemplate.convertAndSend("/subscribe/notification." + cleanSessionId, pushMessageToKafka);
+
+            log.info("dto ===> " + pushMessageToKafka);
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex); // 추후에 GlobalException 처리
         }
     }
 }
