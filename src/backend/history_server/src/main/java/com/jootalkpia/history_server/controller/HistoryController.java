62ce423da6eb1233d3cf81fae3b5209511dfaf6e
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-20 15:09:03 +0900

    #225 refactor(be): 주석 수정

--- src/backend/history_server/src/main/java/com/jootalkpia/history_server/controller/HistoryController.java
+++ src/backend/history_server/src/main/java/com/jootalkpia/history_server/controller/HistoryController.java
@@ -20,9 +20,13 @@ public class HistoryController {
     @GetMapping("/api/v1/history/{channelId}")
     public ChatMessagePageResponse getChatMessagesForward(
             @PathVariable Long channelId,
-            @RequestParam(required = false) Long cursorId,    //처음 요청시엔 서버 내에서 안읽은 메세지 값으로 설정하기 위해
+            //처음 요청시엔 서버 내에서 안읽은 메세지 값으로 설정하기 위해 false
+            // 이때 message의 objectId가 아닌 threadId가 기준
+            @RequestParam(required = false) Long cursorId,
             @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int size,
             @CurrentUser UserInfo userInfo) {
-        return historyQueryService.getChatMessagesForward(channelId, cursorId, size, userInfo.userId());
+        System.out.println(cursorId);
+        return historyQueryService.
+                getChatMessagesForward(channelId, cursorId, size, userInfo.userId());
     }
 }
