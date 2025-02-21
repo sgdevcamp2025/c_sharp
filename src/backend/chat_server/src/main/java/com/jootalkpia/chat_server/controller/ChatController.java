3e298c6d41c5a1098c8fb88f3ed323e06eaaf71a
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-21 17:08:13 +0900

    #264 feat(be): 로드밸런싱 확인을 위한 controller 구현

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/controller/ChatController.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/controller/ChatController.java
@@ -2,9 +2,12 @@ package com.jootalkpia.chat_server.controller;
 
 import com.jootalkpia.chat_server.dto.messgaeDto.ChatMessageRequest;
 import com.jootalkpia.chat_server.service.ChatService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +20,11 @@ public class ChatController {
     public void sendMessage(ChatMessageRequest request, @DestinationVariable Long channelId) {
         chatService.processChatMessage(request, channelId);
     }
+
+    @GetMapping("/api/chat-port")
+    public ResponseEntity<String> getWebSocketPort(HttpServletRequest request) {
+        int port = request.getServerPort(); // 현재 서버의 포트 가져오기
+        return ResponseEntity.ok(String.valueOf(port));
+    }
+
 }
