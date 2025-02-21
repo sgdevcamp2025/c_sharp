c6325cff432444880201bffc1f4fa19b867ea4a0
Sonjieun <sonjieun2661@gmail.com>
2025-02-22 00:47:17 +0900

    화면 스타일링

--- src/frontend/apps/web/app/test/page.tsx
+++ src/frontend/apps/web/app/test/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useRef, useState } from 'react';
+
 const STOMP_SERVER_URL = 'http//13.125.13.209:8090/ws';
 const RTC_CONFIGURATION = {
   iceServers: [
@@ -14,5 +18,76 @@ const RTC_CONFIGURATION = {
 };
 
 export default function page() {
-  return <div>page</div>;
+  //유저id 입력, 채널 id입력을 위한 변수
+  const [userId, setUserId] = useState('');
+  const [channelId, setChannelId] = useState('');
+
+  //방참가 여부
+  const [isInCall, setIsInCall] = useState(false);
+
+  //stomp연결
+  const [stompClient, setStompClient] = useState(null);
+
+  //참여자 목록
+  const participants = useRef<{ [key: string]: any }>({});
+
+  //다른참가자 미디어 스트림 목록
+  const videoRefs = useRef<{ [key: string]: HTMLVideoElement | null }>({});
+
+  //내 미디어 스트림
+  const localVideoRef = useRef<HTMLVideoElement | null>(null);
+
+  return (
+    <div>
+      <h2>Kurento WebRTC Group Call (SockJS + STOMP + STUN/TURN)</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="유저아이디"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="채널"
+          value={channelId}
+          onChange={(e) => setChannelId(e.target.value)}
+        />
+        {/* 방에 참가가 되면, 나오는 버튼 */}
+        {!isInCall ? (
+          <button onClick={joinRoom}>Join Room</button>
+        ) : (
+          <button onClick={leaveRoom}>Leave Room</button>
+        )}
+      </div>
+      {/* 내 화면 */}
+      <div>
+        <video
+          ref={localVideoRef}
+          autoPlay
+          muted
+        />
+      </div>
+      {/* 다른 참가자들 화면 */}
+      <div id="remoteVideos">
+        {Object.keys(videoRefs.current).map((peerId) => (
+          <video
+            key={peerId}
+            ref={(ref) => {
+              if (ref) {
+                videoRefs.current[peerId] = ref;
+              }
+            }}
+            autoPlay
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
+
+//방참가 (방 생성)
+const joinRoom = () => {};
+
+//방 나가기
+const leaveRoom = (e) => {};
