5ff6e94899cdfaef86cfa128bab633da74cab2fc
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-21 17:02:34 +0900

    #264 refactor(be): 불필요 주석 삭제

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/controller/ChatController.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/controller/ChatController.java
@@ -1,11 +1,7 @@
 package com.jootalkpia.chat_server.controller;
 
 import com.jootalkpia.chat_server.dto.messgaeDto.ChatMessageRequest;
-import com.jootalkpia.chat_server.dto.ChatMessageToKafka;
-import com.jootalkpia.chat_server.dto.messgaeDto.CommonResponse;
 import com.jootalkpia.chat_server.service.ChatService;
-import com.jootalkpia.chat_server.service.KafkaProducer;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
