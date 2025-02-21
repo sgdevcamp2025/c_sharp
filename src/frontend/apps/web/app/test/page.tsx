7d53e3e3693d99d4c3694416279d8a2405f3d94a
Sonjieun <sonjieun2661@gmail.com>
2025-02-22 00:43:01 +0900

    상수 선언

--- src/frontend/apps/web/app/test/page.tsx
+++ src/frontend/apps/web/app/test/page.tsx
@@ -1,3 +1,18 @@
+const STOMP_SERVER_URL = 'http//13.125.13.209:8090/ws';
+const RTC_CONFIGURATION = {
+  iceServers: [
+    { urls: 'stun:13.125.13.209:3478' },
+    {
+      urls: 'turn:13.125.13.209:3478?transport=udp',
+      username: 'kurentouser',
+      credential: 'kurentopassword',
+    },
+  ],
+  iceTransportPolicy: 'all',
+  bundlePolicy: 'max-bundle',
+  iceCandidatePoolSize: 0,
+};
+
 export default function page() {
   return <div>page</div>;
 }
