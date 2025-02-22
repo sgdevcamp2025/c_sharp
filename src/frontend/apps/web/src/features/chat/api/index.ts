39beaca83ac8ebf969aa691f445bc38135a71caa
KimKyuHoi <k546kh@gmail.com>
2025-02-22 23:18:25 +0900

    #213 feat(fe): 파일 업로드 후 썸네일 업로드 로직 구현

---
+++ src/frontend/apps/web/src/features/chat/api/index.ts
@@ -0,0 +1,5 @@
+export { getPing } from './get-ping.api';
+export { uploadFiles } from './upload-file.api';
+export { uploadSmallFiles } from './upload-smallfile.api';
+export { uploadChunksQueue } from './upload-chunks-queue.api';
+export { uploadThumbnail } from './upload-thumbnail.api';
