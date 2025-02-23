4450b5b58cff3fc306d404d3d2acca732de3e14b
KimKyuHoi <k546kh@gmail.com>
2025-02-23 14:10:38 +0900

    #213 build(fe): id Index값 추가

--- src/frontend/apps/web/src/features/chat/ui/file-preview-list.tsx
+++ src/frontend/apps/web/src/features/chat/ui/file-preview-list.tsx
@@ -24,6 +24,7 @@ export const FilePreviewList = ({
         {selectedFiles.map((fileData, index) => (
           <FilePreviewItem
             key={index}
+            id={index}
             fileData={fileData}
             onRemove={onRemoveFile}
           />
