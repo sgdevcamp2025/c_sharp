0087fce14dcd15b2b6d104888310209a0413341b
Sonjieun <sonjieun2661@gmail.com>
2025-02-23 14:40:39 +0900

    #259 fix(fe): zustand 문법오류 수정

--- src/frontend/apps/web/src/entities/user/model/user-store.ts
+++ src/frontend/apps/web/src/entities/user/model/user-store.ts
@@ -5,13 +5,13 @@ type UserStore = {
   user: User | null;
   setUser: (user: User | null) => void;
 };
-export const useUserStore = create<UserStore>(() => ({
+export const useUserStore = create<UserStore>((set) => ({
   user:
     typeof window !== 'undefined'
       ? JSON.parse(localStorage.getItem('user') || 'null')
       : null,
   setUser: (user) => {
     localStorage.setItem('user', JSON.stringify(user));
-    return { user };
+    set(() => ({ user }));
   },
 }));
