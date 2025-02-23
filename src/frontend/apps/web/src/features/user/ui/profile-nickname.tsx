e839c4d10210a6922932772a573c30e42d168ffa
Sonjieun <sonjieun2661@gmail.com>
2025-02-24 00:21:35 +0900

    #260 feat(fe): 닉네임 수정 모드 및 input 상태 추가

--- src/frontend/apps/web/src/features/user/ui/profile-nickname.tsx
+++ src/frontend/apps/web/src/features/user/ui/profile-nickname.tsx
@@ -3,11 +3,21 @@
 import { useUserStore } from '@/src/entities';
 import { Button, Input, Label } from '@workspace/ui/components';
 import { Pencil } from 'lucide-react';
+import { useState } from 'react';
 
 const ProfileNickname = () => {
-  const isNameEditMode = false;
   const nickname = useUserStore((state) => state.user?.nickname ?? '');
 
+  const [isNameEditMode, setIsNameEditMode] = useState(false);
+  const [newNickname, setNewNickname] = useState(nickname);
+
+  const handleNameEdit = async () => {
+    if (isNameEditMode) {
+      console.log('changing..');
+      console.log(newNickname);
+    }
+    setIsNameEditMode((prev) => !prev);
+  };
   return (
     <div className="flex w-full  items-center space-x-2">
       <Label htmlFor="name">Name</Label>
@@ -15,11 +25,13 @@ const ProfileNickname = () => {
         id="name"
         disabled={!isNameEditMode}
         placeholder="Enter your name"
-        value={nickname}
+        value={newNickname}
+        onChange={(e) => setNewNickname(e.target.value)}
       />
       <Button
         variant="ghost"
         size="sm"
+        onClick={handleNameEdit}
       >
         {isNameEditMode ? '저장' : <Pencil />}
       </Button>
