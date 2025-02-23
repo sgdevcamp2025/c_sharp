7409a5b3631d3f6ed3df04ce8caa329eea0ce57e
Sonjieun <sonjieun2661@gmail.com>
2025-02-23 17:55:44 +0900

    #273 feat(fe) : 캐싱된 주식 데이터 불러오기

--- src/frontend/apps/web/src/features/stock/ui/stock-chart-container.tsx
+++ src/frontend/apps/web/src/features/stock/ui/stock-chart-container.tsx
@@ -8,14 +8,24 @@ import {
 import StockChartItem from './stock-chart-item';
 import { ChartType, dummyStockData, useStockWebSocket } from '../model';
 import { useEffect } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import { QUERY_KEYS } from '@/src/shared/services';
 
 const StockChartContainer = () => {
+  const queryClient = useQueryClient();
   const data = dummyStockData;
   const { subscribe } = useStockWebSocket();
 
   useEffect(() => {
     return subscribe();
   }, [subscribe]);
+
+  const stockData = queryClient
+    .getQueryData<any[]>(QUERY_KEYS.stocks())
+    ?.filter((data) => data.code === '035420');
+  const stocksDataForTest = queryClient.getQueryData<any[]>(
+    QUERY_KEYS.stocks(),
+  );
 
   return (
     <ResizablePanelGroup direction="vertical">
