d97f7d8cbbd3319c364a85f1b6581d53d06ef533
Sonjieun <sonjieun2661@gmail.com>
2025-02-24 14:54:06 +0900

    #62 주식리스트 쿼리 키 정의

--- src/frontend/apps/web/src/shared/services/apis/querykey.ts
+++ src/frontend/apps/web/src/shared/services/apis/querykey.ts
@@ -1,4 +1,6 @@
 export const QUERY_KEYS = {
   messages: (channelId: number) =>
     ['messages', `/subscribe/chat.${channelId}`] as const,
+  stockList: () => ['stockList'] as const,
+  stockData: (code: string) => ['stockData', code] as const,
 };
