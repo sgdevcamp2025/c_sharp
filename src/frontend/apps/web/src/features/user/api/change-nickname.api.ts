cde1b3792eafe088fefc8021bc7bcc5cc6df8aa7
Sonjieun <sonjieun2661@gmail.com>
2025-02-25 01:27:35 +0900

    #260 fix(fe):api response 형식 변경 및 타입 추가

--- src/frontend/apps/web/src/features/user/api/change-nickname.api.ts
+++ src/frontend/apps/web/src/features/user/api/change-nickname.api.ts
@@ -1,21 +1,27 @@
 'use server';
 
+import { User } from '@/src/entities';
 import { patchRequest } from '@/src/shared/services';
 import { setCookie } from '@/src/shared/services/lib';
 
+type Response = User;
+type Body = {
+  nickname: string;
+};
+
 export const changeNickname = async (nickname: string) => {
   try {
-    const { user } = await patchRequest<any, any>(
+    const user = await patchRequest<Response, Body>(
       'gateway',
       '/api/v1/user/profile',
       { nickname: nickname },
     );
 
-    if (!user || !user.id) {
+    if (!user || !user.userId) {
       throw new Error('User not found');
     }
 
-    setCookie('userId', user.id);
+    setCookie('userId', user.userId);
 
     return user;
   } catch (err) {
