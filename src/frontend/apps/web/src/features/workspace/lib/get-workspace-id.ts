ad755d7cacf028572304e58ea7b2ed0d057e97d3
KimKyuHoi <k546kh@gmail.com>
2025-02-26 02:49:48 +0900

    #281 feat(fe): stockslug에 따른 workspace id값 가져오기 구현

---
+++ src/frontend/apps/web/src/features/workspace/lib/get-workspace-id.ts
@@ -0,0 +1,18 @@
+import { WORKSPACE_ID } from '../model';
+
+export function getWorkspaceId(stockSlug: string): number {
+  switch (stockSlug) {
+    case 'samsung-electronics':
+      return WORKSPACE_ID.SAMSUNG;
+    case 'sk-hynix':
+      return WORKSPACE_ID.SK_HYNIX;
+    case 'kakao':
+      return WORKSPACE_ID.KAKAO;
+    case 'naver':
+      return WORKSPACE_ID.NAVER;
+    case 'hanwha-aerospace':
+      return WORKSPACE_ID.HWANHWA;
+    default:
+      return -1;
+  }
+}
