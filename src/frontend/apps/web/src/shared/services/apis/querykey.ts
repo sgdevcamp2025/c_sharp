754d13830b9e5ec2e1b66d51058c34b3e8db7936
KimKyuHoi <k546kh@gmail.com>
2025-02-26 02:54:14 +0900

    #281 feat(fe): fetch tagkey, react-query query key 분리

--- src/frontend/apps/web/src/shared/services/apis/querykey.ts
+++ src/frontend/apps/web/src/shared/services/apis/querykey.ts
@@ -3,4 +3,8 @@ export const QUERY_KEYS = {
     ['messages', `/subscribe/chat.${channelId}`] as const,
   forwardHistory: (channelId: number) => ['forwardHistory', channelId] as const,
   reverseHistory: (channelId: number) => ['reverseHistory', channelId] as const,
+  workspaceList: (workspaceId: number) =>
+    ['workspaceList', workspaceId] as const,
+  workspaceMessages: (workspaceId: number) =>
+    ['workspaceMessages', `/subscribe/workspace.${workspaceId}`] as const,
 };
