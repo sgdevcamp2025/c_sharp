63729d9c4fbecd15ecb0380f43b7896024939f17
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-02-26 09:03:51 +0900

    #300 feat(be): 채팅 발행 부하테스트를 위한 임시 컨트롤러 구현

--- src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/controller/ChatController.java
+++ src/backend/chat_server/src/main/java/com/jootalkpia/chat_server/controller/ChatController.java
@@ -1,14 +1,11 @@
 package com.jootalkpia.chat_server.controller;
 
 import com.jootalkpia.chat_server.dto.messgaeDto.ChatMessageRequest;
-import com.jootalkpia.chat_server.dto.ChatMessageToKafka;
-import com.jootalkpia.chat_server.dto.messgaeDto.CommonResponse;
 import com.jootalkpia.chat_server.service.ChatService;
-import com.jootalkpia.chat_server.service.KafkaProducer;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +16,11 @@ public class ChatController {
 
     @MessageMapping("/chat.{channelId}")
     public void sendMessage(ChatMessageRequest request, @DestinationVariable Long channelId) {
+        chatService.processChatMessage(request, channelId);
+    }
+
+    @PostMapping
+    public void sendMessageApi(ChatMessageRequest request, @DestinationVariable Long channelId) {
         chatService.processChatMessage(request, channelId);
     }
 }
