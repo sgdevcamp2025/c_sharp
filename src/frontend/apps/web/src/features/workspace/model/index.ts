cd4c72ff76891c5a27207b55b456b571101e488d
KimKyuHoi <k546kh@gmail.com>
2025-02-27 17:40:50 +0900

    #313 build(fe): 코드 모듈화

--- src/frontend/apps/web/src/features/workspace/model/index.ts
+++ src/frontend/apps/web/src/features/workspace/model/index.ts
@@ -1,6 +1,11 @@
 export { WORKSPACE_ID } from './workspace-id';
 export { WorkspaceFormSchema } from './workspace-form-schema';
-export type { WorkspaceSubscriptionResponse } from './subscription.type';
+export type {
+  WorkspaceSubscriptionResponse,
+  UnreadSubscriptionResponse,
+} from './subscription.type';
 export { useWorkspaceMessages } from './use-workspace-messages';
 export { useWorkspaceSubscription } from './use-workspace-subscription';
 export { useWorkspaceChannels } from './use-workspace-channels';
+export { useUnreadMessages } from './use-unread-messages';
+export { useUnreadSubscription } from './use-unread-subscription';
