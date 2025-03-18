076387672d27b704bca2fb6072f729a2c04f38b7
KimKyuHoi <k546kh@gmail.com>
2025-03-18 11:33:45 +0900

    #351 feat(fe): chunk 최적화 간단화

--- src/frontend/apps/web/next.config.mjs
+++ src/frontend/apps/web/next.config.mjs
@@ -1,31 +1,31 @@
 // import withBundleAnalyzer from '@next/bundle-analyzer';
 import TerserPlugin from 'terser-webpack-plugin';
 
-const mode = process.env.NODE_ENV || 'development';
+const mode = process.env.NEXT_NODE_ENV;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@workspace/ui'],
   images: {
+    // eslint-disable-next-line no-undef
     domains: [process.env.NEXT_PUBLIC_S3_BUCKET_NAME || 'default-domain.com'],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer && mode === 'production') {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-      };
-
-      config.optimization.minimizer = [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-            },
-          },
-        }),
-      ];
-    }
-    return config;
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimizer:
+      mode === 'production'
+        ? [
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true,
+                },
+              },
+            }),
+          ]
+        : [],
   },
 };
 
