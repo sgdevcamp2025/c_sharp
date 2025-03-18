212f2baa261b40c8b940ac8205a792f537c4f884
KimKyuHoi <k546kh@gmail.com>
2025-03-18 11:25:54 +0900

    #351 feat(fe): bundlyzer 추가 및 next.js webpack 최적화

--- src/frontend/apps/web/next.config.mjs
+++ src/frontend/apps/web/next.config.mjs
@@ -1,13 +1,58 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import TerserPlugin from 'terser-webpack-plugin';
+
+const mode = process.env.NODE_ENV || 'development';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@workspace/ui'],
   images: {
-    // eslint-disable-next-line no-undef
     domains: [process.env.NEXT_PUBLIC_S3_BUCKET_NAME || 'default-domain.com'],
   },
-  generateBuildId: async () => {
-    return 'my-fixed-build-id';
+  webpack: (config, { isServer }) => {
+    if (!isServer && mode === 'production') {
+      // 커스텀 splitChunks 설정
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        automaticNameDelimiter: '~',
+        cacheGroups: {
+          reactVendors: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendors',
+            chunks: 'all',
+            priority: 20,
+          },
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+            priority: -10,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      };
+
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+          },
+        }),
+      ];
+    }
+    return config;
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
