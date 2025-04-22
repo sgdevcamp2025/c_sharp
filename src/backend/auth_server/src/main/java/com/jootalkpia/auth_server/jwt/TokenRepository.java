b7a7f3e7874bceacc98b8ad2af6ff7321557b2e1
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-04-22 22:32:43 +0900

    #353 del(be) :TokenRepository 불필요 메소드 삭제

--- src/backend/auth_server/src/main/java/com/jootalkpia/auth_server/jwt/TokenRepository.java
+++ src/backend/auth_server/src/main/java/com/jootalkpia/auth_server/jwt/TokenRepository.java
@@ -5,8 +5,5 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TokenRepository extends CrudRepository<Token, Long> {
-
     Optional<Token> findByRefreshToken(final String refreshToken);
-
-    Optional<Token> findById(final Long id);
 }
