83563d244a56e825bc60a2166f870eb50cd1c6ff
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2025-04-22 21:23:30 +0900

    #353 feat(be) :application jpa에서 webflux 구조로 변경

--- src/backend/auth_server/src/main/java/com/jootalkpia/auth_server/AuthServerApplication.java
+++ src/backend/auth_server/src/main/java/com/jootalkpia/auth_server/AuthServerApplication.java
@@ -3,18 +3,30 @@ package com.jootalkpia.auth_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@EnableJpaAuditing
+@EnableR2dbcAuditing
 @EnableFeignClients
 @ImportAutoConfiguration(FeignAutoConfiguration.class)
-@SpringBootApplication(scanBasePackages = {
-        "com.jootalkpia.auth_server",
-        "com.jootalkpia.passport",
-        "com.jootalkpia.config",
-})
+@EnableR2dbcRepositories(basePackages = "com.jootalkpia.auth_server.user.repository")
+@EnableRedisRepositories(basePackages = "com.jootalkpia.auth_server.jwt")
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.jootalkpia.auth_server",
+                "com.jootalkpia.passport",
+                "com.jootalkpia.config",
+        },
+        exclude = {
+                SecurityAutoConfiguration.class,
+                UserDetailsServiceAutoConfiguration.class
+        }
+)
 public class AuthServerApplication {
 
     public static void main(String[] args) {
